132e3b40ec2bfda4d5a3f829330330bd7d11ddf0
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-04 00:28:38 +0000

    update

--- src/App.js
+++ src/App.js
@@ -75,77 +75,82 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    this.setState({box: box});
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch('https://smart-brain-back-end-vmuu.onrender.com/imageurl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
+    this.setState({ imageUrl: this.state.input });
+
+    fetch("https://smart-brain-back-end-vmuu.onrender.com/imageurl", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      }),
+    })
       .then(response => response.json())
       .then(response => {
-        if (response) {
-          fetch('https://smart-brain-back-end-vmuu.onrender.com/image', {
+        // Check if the response contains an error
+        if (response.error) {
+          console.error('Clarifai API Error:', response.error);
+          // Handle the error case, e.g., show a message to the user
+          // You may want to update the state or UI accordingly
+        } else {
+          // Proceed with face detection and image update
+          fetch("https://smart-brain-back-end-vmuu.onrender.com/image", {
             method: 'put',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               id: this.state.user.id
             })
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-            .catch(console.log)
+            .catch(console.log);
 
+          this.displayFaceBox(this.calculateFaceLocation(response));
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
       })
-      .catch(err => console.log(err));
-  }
+      .catch(err => {
+        console.error('Fetch Error:', err);
+        // Handle fetch error, e.g., show a message to the user
+        // You may want to update the state or UI accordingly
+      });
+  };
 
   onRouteChange = (route) => {
     if (route === 'signout') {
       this.setState(initialState)
     } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+      this.setState({ isSignedIn: true })
     }
-    this.setState({route: route});
+    this.setState({ route: route });
   }
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
-      <div className="App">
-        <ParticlesBg type="fountain" bg={true} />
+      <div className='App'>
+        <ParticlesBg type="cobweb" bg={true} />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home'
+        {route === 'home'
           ? <div>
-              <Logo />
-              <Rank
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
           : (
-             route === 'signin'
-             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
+            route === 'SignIn'
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
