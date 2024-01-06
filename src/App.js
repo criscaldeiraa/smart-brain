86591c8c2db67f5f831eb31674c9a2d58bc07052
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-06 00:18:24 +0000

    updating

--- src/App.js
+++ src/App.js
@@ -147,7 +147,7 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
           : (
-            route === 'SignIn'
+            route === 'signin'
               ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
