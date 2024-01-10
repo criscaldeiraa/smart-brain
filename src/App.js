a708d937a3533750956005e923bd519683d2dd1d
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-10 18:59:04 +0000

    clarifai image

--- src/App.js
+++ src/App.js
@@ -56,7 +56,25 @@ class App extends Component {
     //   const image = document.getElementById('inputimage');
     //   const width = Number(image.width);
     //   const height = Number(image.height);
+    const regions = response.outputs[0].data.regions;
 
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+
+            })
+          })
       return {
         leftCol: boundingBox.left_col * width,
         topRow: boundingBox.top_row * height,
