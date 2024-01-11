2f0d7c5e4a0851f58ede903a387c01355d8cef71
criscaldeiraa <criscaldeirat@gmail.com>
2024-01-11 02:14:13 +0000

    clarifai image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -11,14 +11,31 @@ const FaceRecognition = ({ imageUrl, box }) => {
       image2 = <img id="inputimage" src={imageUrl} alt="" width='500px' height='auto' />;
   }
 
+  function imageBox(props) {
+    const finalBox = box.map((boxItm, idx)=> (
+      <div 
+        key={idx} 
+        className='bounding-box' 
+        style={
+          {
+            top: boxItm.topRow, 
+            right: boxItm.rightCol, 
+            bottom: boxItm.bottomRow, 
+            left: boxItm.leftCol
+            }}>
+          </div>))
+  }
+
+
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         {image2}
-        {box.map((boxItm, idx) => (
+        <imageBox />
+        {/* {box.map((box, idx) => (
           <div key={idx} className='bounding-box' style={{top: boxItm.topRow, right: boxItm.rightCol, bottom: boxItm.bottomRow, left: boxItm.leftCol}}></div>
-        ))} {/*https://stackoverflow.com/a/64827479/10474024*/}
-        {/* <img id='inputimage' alt='' src={imageUrl} width='400px' heigh='auto'/> */}
+        ))} */}
+        {/* <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/> */}
         {/* <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div> */}
       </div>
     </div>
